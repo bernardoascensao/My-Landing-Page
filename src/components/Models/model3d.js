299e69a32eba27a08f11778e8a8b4e03f5b07b4e
Bernardo Ascensão <bernardo.santos007@gmail.com>
2025-12-17 02:08:01 +0000

    Comment unused variable

--- src/components/Models/model3d.js
+++ src/components/Models/model3d.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export const Model3d = () => {
   const canvasRef = useRef(null)
