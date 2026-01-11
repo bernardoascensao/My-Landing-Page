88f3ef89daf4de4c61bd1c51eb426e82eb60e322
Bernardo Ascensão <bernardo.santos007@gmail.com>
2026-01-11 04:19:12 +0000

    profile card black and with glow

--- src/components/Models/profileCard.js
+++ src/components/Models/profileCard.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { FaGithub, FaLinkedin, FaEnvelope, FaPaperclip } from 'react-icons/fa';
 import profile_img from '../../assets/images/profile_picture.jpg';
 import cv from '../../assets/documents/CV_Bernardo_Ascensao_PT.pdf'
@@ -28,23 +27,35 @@ const SocialLinks = () => {
 
 const ProfileCard = () => {
   return (
-    <div className='bg-white w-80 h-80 md:w-96 md:h-96 text-center rounded-lg shadow-lg relative'>
-        <img 
-            src={profile_img}
-            alt="Profile"
-            className='w-48 h-48  md:w-52 md:h-52 rounded-full mx-auto mb-4 shadow-xl absolute -top-24 left-16 md:-top-1/4 md:left-[22.91%]'
-        />
+    <div className="relative">
+      {/* Glow */}
+      <div
+        className="
+          absolute -inset-3
+          rounded-xl
+          bg-blue-500/25
+          blur-2xl
+        "
+      />
+      {/* Card */}
+      <div className='bg-neutral-900 w-80 h-80 md:w-96 md:h-96 text-center rounded-lg shadow-lg relative'>
+          <img 
+              src={profile_img}
+              alt="Profile"
+              className='w-48 h-48 md:w-52 md:h-52 rounded-full mx-auto mb-4 shadow-xl absolute -top-24 left-16 md:-top-1/4 md:left-[22.91%]'
+          />
 
-        <div className='w-full px-5 h-[60%] flex flex-col justify-start absolute top-[40%] left-0'>
-            <div className='border-b border-gray-300 my-4 pb-4'>
-                <h1 className='font-medium text-2xl md:text-3xl'>{data.person.name}</h1>
-                <p className='text-xl md:text-2xl'>{data.person.occupation}</p>
-            </div>
+          <div className='w-full px-5 h-[60%] flex flex-col justify-start absolute top-[40%] left-0'>
+              <div className='border-b border-gray-300 my-4 pb-4'>
+                  <h1 className='font-medium text-2xl md:text-3xl'>{data.person.name}</h1>
+                  <p className='text-xl md:text-2xl'>{data.person.occupation}</p>
+              </div>
 
-            <div className='flex justify-center items-center mt-3 md:mt-6'>
-                <SocialLinks />
-            </div>
-        </div>
+              <div className='flex justify-center items-center mt-3 md:mt-6'>
+                  <SocialLinks />
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
